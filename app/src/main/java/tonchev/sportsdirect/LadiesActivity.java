c130d2f8e37ece1daa6bfa79eeb2ab4b6757ecbc
Tsvetomir Tonchev <cvetomirtonchev@gmail.com>
2017-03-13 23:15:15 +0200

    all clothing in ladies

--- app/src/main/java/tonchev/sportsdirect/LadiesActivity.java
+++ app/src/main/java/tonchev/sportsdirect/LadiesActivity.java
@@ -1,13 +1,20 @@
 package tonchev.sportsdirect;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import shops.Clothing;
 
 public class LadiesActivity extends AppCompatActivity {
+    private Button ladiesClothing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ladies);
+
     }
 }
